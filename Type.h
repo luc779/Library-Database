bd466cc7a906753be18e1d90f3f33e44e0aae7c4
Luc Debaupte <73355545+luc779@users.noreply.github.com>
2022-11-18 20:13:32 -0800

    Add files via upload

---
+++ Type.h
@@ -0,0 +1,20 @@
+//
+//  Type.h
+//  HW5
+//
+//  Created by Luc Debaupte on 11/18/22.
+//
+
+#ifndef Type_h
+#define Type_h
+
+class Type {
+  public:
+    void setType(char argType) { argType = type; }
+    char getType() { return type; }
+  private:
+    char type;
+  
+};
+
+#endif /* Type_h */
